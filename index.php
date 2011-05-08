047419e43f5685c24ec8ec87a05416de94037089
Ryan Holmes <holmes.ryan.90@gmail.com>
2011-05-08 12:41:50 -0400

    generalization. added vars for corpID and corp/alliance ticker rather than hardcode them.

--- index.php
+++ index.php
@@ -20,7 +20,36 @@
 
 ob_start("ob_gzhandler"); 
 
+//**************
+//   Config   //
+//////////////**
+
+$accountKey  = 1002; // the wallet division to pull from. 1000 - 1006 (1000 = master, 1001 = 2nd division, etc)
+$rowCount    = 1000; /* how many rows of data to pull. It has to be enough to cover the entire month for this to be accurate.
+					   If you have 1000 members in corp, and they each donate exactly once per month, this should cover it.
+					   Max is 2560 (if you need more than that, your SOL. It would require a database backend to archive 
+					   data, or some sort of function to walk the journal via fromID. Either way requires considerable 
+					   re-working to the code.) */
+
+$goal        = 10000000; // the amount of tax per month per member
+$corpDiv     = '3rd Wallet Division'; // the name of the wallet division to donate to
+$corpID      = 98022296; // corp ID, used for IGB Show Info button
+$corpTic     = 'M.DYN'; // corp ticker
+$allianceTic = '.SUDO'; // alliance ticker
+
+$leadership = array(    // list some people in the corp who can help other members with questions pertaining to the Alliance Tax
+	'Aurum Pax', 
+	'Hal IV', 
+	'Nimloth Valinor', 
+	'Sable Blitzmann', 
+	'Torvix');
+	
+//******************
+//   End Config   //
+//////////////////**
+
 define('TIMESTART', microtime(true));  // Start page benchmark
+define('VERSION', '1.1.1');  // Start page benchmark
 
 if (isset($_GET['source'])) {
 	// Also, apiDetails sample file can be found in root web. It's named 'apiDetails-sample.ini'
@@ -28,17 +57,10 @@ if (isset($_GET['source'])) {
 	die;
 }
 
-$accountKey = 1002; // the wallet division to pull from. 1000 - 1006 (1000 = master, 1001 = 2nd division, etc)
-$rowCount   = 1000; /* how many rows of data to pull. It has to be enough to cover the entire month for this to be accurate.
-					   If you have 1000 members in corp, and they each donate exactly once per month, this should cover it.
-					   Max is 2560 (if you need more than that, your SOL. It would require a database backend to archive 
-					   data and some considerable re-working to this code.) */
-$goal       = 10000000; // the amount of tax per month per member
-
 $ref = array( // array of different ref values
 	10 => 'Donation',
 	37 => 'Corp Withdrawl');
-
+	
 $members    = array();
 $payedTax   = array();
 $journal    = array();
@@ -51,14 +73,14 @@ unset($apiDeatils);
 
 function get_data($url)
 {
-  $ch = curl_init();
-  $timeout = 5;
-  curl_setopt($ch,CURLOPT_URL,$url);
-  curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
-  curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,$timeout);
-  $data = curl_exec($ch);
-  curl_close($ch);
-  return $data;
+	$ch = curl_init();
+	$timeout = 5;
+	curl_setopt($ch,CURLOPT_URL,$url);
+	curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+	curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,$timeout);
+	$data = curl_exec($ch);
+	curl_close($ch);
+	return $data;
 }
 
 function refreshAPI($filename, $url) {
@@ -147,8 +169,8 @@ ksort($dirtyFreeLoaders);
 <body>
 
 <div class='infoBar'>
-	<button class='button' style='float: left;'<?php echo (strpos($_SERVER['HTTP_USER_AGENT'], 'EVE-IGB') ? " onclick='CCPEVE.showInfo(2, 98022296)'" : " disabled='disabled'"); ?>>M.DYN Show Info</button>
 	<form method='post' style='float: right;' ><button class='button' name='refresh' type='submit'<?php echo (time() < strtotime($membersCache->cachedUntil) && time() < strtotime($walletCache->cachedUntil) ? " disabled='disabled'" : null); ?>>Refresh Now!</button></form>
+	<button class='button' style='float: left;'<?php echo (strpos($_SERVER['HTTP_USER_AGENT'], 'EVE-IGB') ? " onclick='CCPEVE.showInfo(2, $corpID)'" : " disabled='disabled'"); ?>><?php echo $corpTic; ?> Show Info</button>
 	Member API last updated: <strong><?php echo $membersCache->currentTime; ?></strong>; cached until <strong><?php echo $membersCache->cachedUntil; ?></strong> || 
 	Wallet API last updated: <strong><?php echo $walletCache->currentTime; ?></strong>; cached until <strong><?php echo $walletCache->cachedUntil; ?></strong>
 </div>
@@ -157,10 +179,10 @@ ksort($dirtyFreeLoaders);
 <h1>Alliance Tax System</h1>
 
 <h4>Welcome!</h4>
-<p>Welcome to the <tt>M.DYN</tt> Alliance Tax System! Every month, <tt>M.DYN</tt>, along with the other <tt>.SUDO</tt> corps, must pay the alliance 10 million ISK per corp member. This will help fund an alliance reimbursment program for logistics, capitals, and other shiney ships. This little application will help you and the corp leadership keep track of who as payed and who still needs to dontate. For those who haven't donated yet, the last login date date is displayed to better determine whether they just gotten around to it yet or if they dropped off the face of the Earth 3 months ago.</p>
+<p>Welcome to the <tt><?php echo $corpTic; ?></tt> Alliance Tax System! Every month, <tt><?php echo $corpTic; ?></tt>, along with the other <tt><?php echo $allianceTic; ?></tt> corps, must pay the alliance <?php number_format($goal); ?> ISK per corp member. This will help fund an alliance reimbursment program for logistics, capitals, and other shiney ships. This little application will help you and the corp leadership keep track of who as payed and who still needs to dontate. For those who haven't donated yet, the last login date date is displayed to better determine whether they just gotten around to it yet or if they dropped off the face of the Earth 3 months ago.</p>
 
 <h4>How Do I Donate?</h4>
-<p>Simply 'Show Info' on the <tt>M.DYN</tt> corp (in IGB, click button in top-left corner of the page), click the white box in the top left corner, and select 'Give Money'. Set the account to <strong>3rd Wallet Division</strong>, the amount to <strong>10000000</strong> (10 million), and reason to <strong>Alliance Tax</strong>. Click 'OK'. Done! Give it a bit of time, then refresh the API cache by clicking the button in the top right hand corner of this page to verify your payment. <strong>Donate only once a month.</strong> Please don't donate 50mil and think that your set for 5 months, that will just create more overhead for everyone (and you won't get to see your name on the list! you do want that, don't you? =P)</p>
+<p>Simply 'Show Info' on the <tt><?php echo $corpTic; ?></tt> corp (in IGB, click button in top-left corner of the page), click the white box in the top left corner, and select 'Give Money'. Set the account to <strong><?php echo $corpDiv; ?></strong>, the amount to <strong><?php echo $goal; ?></strong>, and reason to <strong>Alliance Tax</strong>. Click 'OK'. Done! Give it a bit of time, then refresh the API cache by clicking the button in the top right hand corner of this page to verify your payment. <strong>Donate only once a month.</strong> Please don't donate 50mil and think that your set for 5 months, that will just create more overhead for everyone (and you won't get to see your name on the list! you do want that, don't you? =P)</p>
 
 <hr />
 <h2 style='text-align:center;'>Month <?php echo $month ?></h2>
@@ -243,7 +265,7 @@ $i++;
 </div>
 <div class='wrapper'>
 <h4>Problems?</h4>
-<p>This is still very new, and it may have bugs. It only works if you've donated to the proper wallet division. If you donated to the master wallet and then moved it over, it's not going to work and you will most likely be left out of the 'payed' list. For this reason, please make sure you donate to the proper wallet division to save everyone the headache. Currently, it's only set up to see if you've donated anything at all, not neccessarily if it was 10 million or more/less. There's a few cavets, but it should work for the most part. Any bugs/questions about the application itself should be directed towards Sable Blitzmann. Any questions or concerns about your monthly Alliance Tax status ("I haz donated to teh wrong wallet, plz halp!", etc.) should be directed to anyone in a leadership position (Aurum Pax, Hal IV, Nimloth Valinor, Sable Blitzmann, Torvix) -- they'll get you sorted out.</p>
+<p>This is still very new, and it may have bugs. It only works if you've donated to the proper wallet division. If you donated to the master wallet and then moved it over, it's not going to work and you will most likely be left out of the 'payed' list. For this reason, please make sure you donate to the proper wallet division to save everyone the headache. Currently, it's only set up to see if you've donated anything at all, not neccessarily if it was 10 million or more/less. There's a few cavets, but it should work for the most part. Any bugs/questions about the application itself should be directed towards Sable Blitzmann. Any questions or concerns about your monthly Alliance Tax status ("I haz donated to teh wrong wallet, plz halp!", etc.) should be directed to anyone in a leadership position (<?php echo implode($leadership, ', '); ?>) -- they'll get you sorted out.</p>
 
 <h4>EVE Mail</h4>
 <p>Here is a nicely put-together string that can be used to easily mail everyone who has not yet payed to remind them to do so. Put it in the "To:" field.<br /><br />
@@ -251,7 +273,7 @@ $i++;
 </p>
 </div>
 <div id='footer'>
-v1.1 | Copyright &copy; 2011 Ryan Holmes | EVE Online &copy; CCP | <?php echo sprintf('%01.002fms', (microtime(true) - TIMESTART) * 1000); ?> | <a href="?source">View Source</a>
+v<?php echo VERSION; ?> | Copyright &copy; 2011 Ryan Holmes/Sable Blitzmann of M.DYN | EVE Online &copy; CCP | <?php echo sprintf('%01.002fms', (microtime(true) - TIMESTART) * 1000); ?> | <a href="?source">View Source</a>
 </div>
 </body>
 </html>	
